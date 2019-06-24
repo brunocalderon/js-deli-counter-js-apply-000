1add32f2ef621206d1f3bbeadda355ae8bf67f09
Bruno Calderon <b@brunocalderon.com>
2019-06-24 17:28:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ let currentTicket = 100;
 function takeANumberWithoutPerson(inline) {
   let peopleAhead = inline.length + 1;
   inline.push(currentTicket);
+  currentTicket + 1;
   customerTicket = currentTicket + 1;
   return `Welcome. You are number ${peopleAhead} in line, and your ticket number is ${customerTicket}`;
 }
